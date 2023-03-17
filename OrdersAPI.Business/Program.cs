a54f7759bb2d55fe65f3a87abea4f26fb1c9c56c
Patrick José <patric-jose@hotmail.com>
2023-03-17 17:27:28 -0300

    Program.cs

---
+++ OrdersAPI.Business/Program.cs
@@ -0,0 +1,33 @@
+using OrdersAPI.Business;
+using OrdersAPI.Data.Repository;
+using OrdersAPI.Utils;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ILogWriter, LogWriter>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderBusiness, OrderBusiness>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
